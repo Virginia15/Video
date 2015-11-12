8ad8321dab72734f7dcbae3a5433f4a6c77eb1ba
JesúsAlejandro <JessAlejandro@ALEJANDRO>
2015-11-12 14:12:15 -0500

    primero

---
+++ Video/src/video/Video.java
@@ -0,0 +1,12 @@
+
+package video;
+/**
+ *
+ * @author JesúsAlejandro
+ */
+public class Video {
+
+    public static void main(String[] args) {
+        System.out.println("sout tab solucionan todo :v");
+    }
+}
